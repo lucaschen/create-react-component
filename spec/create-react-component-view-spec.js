89e11dd3f6b16953e05692987d332822e9d85fe2
Lucas Chen <2109121+parkroolucas@users.noreply.github.com>
2018-07-26 12:07:06 +1000

    feature
    Initial commit; added support for class-based and stateless React
    component generation

---
+++ spec/create-react-component-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import CreateReactComponentView from '../lib/create-react-component-view';
+
+describe('CreateReactComponentView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
